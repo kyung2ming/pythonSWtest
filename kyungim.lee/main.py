2699afd813529ad6837708fc545dd1a058500cb6
kyungim lee <kyungim.lee@koscom.co.kr>
2021-03-18 17:08:38 +0900

    11053

--- kyungim.lee/main.py
+++ kyungim.lee/main.py
@@ -5,7 +5,7 @@ from sys import stdin
 # memo : Ai를 last value로 갖는 LIS
 memo = [0 for _ in range(1111)]
 
-if __name__ ==  '__main__':
+if __name__ == '__main__':
 
     # 수열 A의 크기
     N = int(stdin.readline().strip())
@@ -13,13 +13,15 @@ if __name__ ==  '__main__':
     # 수열 A를 이루는 Ai
     A = [map(int, stdin.readline().strip().split(" ")) for _ in range(N)]
 
-    memo[0]= 1
+    memo[0] = 1
+    _max = 0
+
     for i in range(N):
         memo[i] = 1
         for j in range(i):
-            if array[i] > array[j] and memo[j] + 1 > memo[i]:
+            if A[i] > A[j] and memo[j] + 1 > memo[i]:
                 memo[i] = memo[j] +1
-        if max < memo[i]:
-            max = memo[i]
+        if _max < memo[i]:
+            _max = memo[i]
 
-    print(max)
+    print(_max)
